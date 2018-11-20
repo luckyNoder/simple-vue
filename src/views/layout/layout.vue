23c91adf2767fda2d362898ab73c1a843af9789f
mifan <luckyNoder@163.com>
2018-11-20 11:28:02 +0800

    首页布局

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,3 +1,66 @@
 <template>
-  <div>layout</div>
+  <div class="layout_warpper">
+    <Layout class="layout_warpper_autho">
+      <Sider collapsible v-model="collapsible" hide-trigger breakpoint="sm"></Sider>
+      <Layout>
+        <Header class="header_wrapper">
+           <Icon :class="collType" @click.native="handleColl" type="md-menu" size="32"></Icon>
+        </Header>
+        <Content class="content_wrapper">
+          <Card shadow bordered class="content_pages">
+            //
+            <!-- <router-view><router-view/> -->
+          </Card>
+        </Content>
+      </Layout>
+    </Layout>
+  </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      collapsible:false
+    }
+  },
+  computed:{
+    collType(){
+      return [
+        "collIcon",
+        this.collapsible? 'rotale':''
+      ]
+    }
+  },
+  methods:{
+    handleColl(){
+      this.collapsible = !this.collapsible
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.layout_warpper,.layout_warpper_autho{
+  height: 100%;
+  .header_wrapper{
+    background: #fff;
+    box-shadow: 0 1px 1px 1px rgba(0, 0, 0, .1);
+    padding: 0 15px;
+    .collIcon{
+      cursor: pointer;
+      transition: all .3s ease;
+      &.rotale{
+        transform:rotateZ(-90deg);
+        transition: all .3s ease;
+      }
+    }
+  }
+  .content_wrapper{
+    padding: 10px;
+    .content_pages{
+      min-height: calc(~"100vh - 84px")
+    }
+  }
+}
+</style>
+
