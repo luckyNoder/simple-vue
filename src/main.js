ad32b0dbacae3081c7241e36a7c2b70d47540115
mifan <luckyNoder@163.com>
2018-11-20 10:38:07 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import store from './store'
+import iView from 'iview'
 
+// css
+import 'iview/dist/styles/iview.css'
+import 'normalize.css'
 Vue.config.productionTip = false
-
+Vue.use(iView)
 new Vue({
   router,
   store,
